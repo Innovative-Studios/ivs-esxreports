5d5fc869f33220755672a6b8fd8a47f4d0c4d522
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-06-25 00:15:24 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'aidanohart'
+description 'SQL Reports System made for Laborious Roleplay'
+version '1.0'
+
+server_script 'server/main.lua'
+client_script 'client/client.lua'
+
