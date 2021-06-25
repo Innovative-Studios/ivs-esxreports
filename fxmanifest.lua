1cd5b663f41af572e85268f7de81cabf77f8e62a
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-06-25 03:15:18 +0100

    MySQL Functionality

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,11 @@ author 'aidanohart'
 description 'SQL Reports System made for Laborious Roleplay'
 version '1.0'
 
-server_script 'server/main.lua'
+server_script {
+    '@mysql-async/lib/MySQL.lua',
+    'server/main.lua',
+}
+
+
 client_script 'client/client.lua'
 
