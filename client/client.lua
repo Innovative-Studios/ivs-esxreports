e2f4a8b06dcd0135d6535c4a917709a62f7416e3
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-06-25 03:15:08 +0100

    Added chat suggestions

--- client/client.lua
+++ client/client.lua
@@ -6,3 +6,11 @@ Citizen.CreateThread(function()
         Citizen.Wait(10)
     end
 end)
+
+Citizen.CreateThread(function()
+
+	TriggerEvent('chat:addSuggestion', '/report', 'Creates a report that gets sent to the staff', {{ name="Reason", help="Please write a detailed reason on your report to go to out staff"}})
+	TriggerEvent('chat:addSuggestion', '/openreports', 'Displays all open reports')
+	TriggerEvent('chat:addSuggestion', '/closereport', 'Closes open reports', {{ name="ID", help="Report ID (This is displayed on /openreports command)" },{ name="Reason", help="Close Reason" }})	
+
+end)
