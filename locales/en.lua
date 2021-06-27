077f7e635e2c77d093ba269ca674762584902501
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-06-27 02:26:23 +0100

    Created english locale file

---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+
+    ['admin_alert'] = '[^1Report^0] ^1^*A NEW REPORT HAS BEEN SUBMITTED. ',
+    ['wrong_format'] = '[^1Report^0] Ensure you follow the format for a report » /report <Reason>',
+    ['no_open_reports'] = '[^1Report^0] There are currently no open reports',
+    ['no_permissions'] = '[^1Report^0] You do not have permission to execute that command',
+    ['already_closed'] = '[^1Report^0] This report is already closed',
+    ['enter_a_reason'] = '[^1Report^0] Please enter a reason to close the report',
+    ['report_submitted'] = '[^1Report^0] Your report has been submitted',
+    ['report_closed'] = '[^1Report^0] You have closed the report ^1ID %s ^0for the reason: ^1 %s',  -- The first %s shows the report ID, the second shows the close reason
+
+}
