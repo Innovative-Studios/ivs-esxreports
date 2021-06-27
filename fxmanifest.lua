1e2466820f8f2dad15dc593f4301f102429825b1
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-06-27 02:26:15 +0100

    Updated manifest for locale support

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,18 @@ fx_version 'cerulean'
 game 'gta5'
 author 'aidanohart'
 description 'SQL Reports System made for Laborious Roleplay'
-version '1.0'
+version '1.1.0'
 
 server_script {
     '@mysql-async/lib/MySQL.lua',
     'server/main.lua',
 }
 
+shared_scripts {
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'config.lua',
+}
 
-client_script 'client/client.lua'
+    client_script 'client/client.lua'
 
